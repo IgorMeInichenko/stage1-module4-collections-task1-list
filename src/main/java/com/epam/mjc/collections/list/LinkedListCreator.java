8515a8a361a242e1c53e2c7492797e712a7f4138
Igor Melnichenko <imelnicenko498@gmail.com>
2022-12-06 11:24:01 +0300

    All ready

--- src/main/java/com/epam/mjc/collections/list/LinkedListCreator.java
+++ src/main/java/com/epam/mjc/collections/list/LinkedListCreator.java
@@ -5,5 +5,15 @@ import java.util.List;
 
 public class LinkedListCreator {
     public LinkedList<Integer> createLinkedList(List<Integer> sourceList) {
+        LinkedList<Integer> result = new LinkedList<>();
+        for (Integer x: sourceList){
+            if (x%2==0){
+                result.add(result.size(),x);
+            }
+            else{
+                result.add(0,x);
+            }
+        }
+        return result;
     }
 }
--- src/main/java/com/epam/mjc/collections/list/ListSorter.java
+++ src/main/java/com/epam/mjc/collections/list/ListSorter.java
@@ -1,15 +1,41 @@
 package com.epam.mjc.collections.list;
 
-import java.util.Comparator;
-import java.util.List;
+import java.util.*;
 
 public class ListSorter {
     public void sort(List<String> sourceList) {
+        for (int i=0;i<sourceList.size()-1;++i){
+            for(int j=0;j<sourceList.size()-i-1; ++j){
+                int compareResult = new ListComparator().compare(sourceList.get(j),sourceList.get(j+1));
+                if (compareResult==1){
+                    Collections.swap(sourceList,j,j+1);
+                }
+
+            }
+        }
     }
 }
 
 class ListComparator implements Comparator<String> {
     @Override
     public int compare(String a, String b) {
+        int aInt = (int) Integer.parseInt(a);
+        int bInt = (int) Integer.parseInt(b);
+        int aRes = func(aInt);
+        int bRes = func(bInt);
+        if (aRes==bRes){
+            if (aInt==bInt){
+                return 0;
+            }
+            if (aInt>bInt){
+                return 1;
+            }
+        }
+        if (aRes>bRes)
+            return 1;
+        return -1;
+    }
+    public int func(int x){
+        return (5*x*x+3);
     }
 }
